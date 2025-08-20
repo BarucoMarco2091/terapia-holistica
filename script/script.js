c5f719e3d2661513ebbe59b9d59f4db60922755a
Marco <marcobaruco2091@gmail.com>
2025-08-20 13:22:00 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+lucide.createIcons();
+
+$(document).ready(function () {
+    $('#mobile_btn').on('click', function() {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-bars fa-x');
+    });
+});
